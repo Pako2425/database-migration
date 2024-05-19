33be4672ba0494e755a7afb6ba19fb1dcbd487f2
Pako2425 <patryk.kocinski340@gmail.com>
2024-05-19 23:06:06 +0200

    connected with mysql database. Flyway connected. Init file for init database. First migration.

---
+++ database-migration/src/main/resources/db/migration/V1_0__init-db.sql
@@ -0,0 +1,6 @@
+create table suppliers(
+    id int primary key auto_increment,
+    company_name varchar(50),
+    country varchar(50),
+    join_date varchar(4)
+);
---
+++ database-migration/src/test/java/com/databasemigration/databasemigration/DatabaseMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databasemigration.databasemigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
